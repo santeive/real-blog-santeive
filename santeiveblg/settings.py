979f49c65a5f6a42f6203e9b709658244c92d8db
santeive <packo.ive@gmail.com>
2020-08-02 13:04:50 -0500

    statis files

--- santeiveblg/settings.py
+++ santeiveblg/settings.py
@@ -115,6 +115,11 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+
+STATIC_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
