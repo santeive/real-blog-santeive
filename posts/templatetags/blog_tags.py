8a788e29e12e716d99831dc7e3ea3aedff288dd8
santeive <packo.ive@gmail.com>
2020-08-04 19:01:29 -0500

    lastest post and similar posts

---
+++ posts/templatetags/blog_tags.py
@@ -0,0 +1,25 @@
+ 
+from django.utils.safestring import mark_safe
+import markdown
+from django import template
+from ..models import Post
+
+register = template.Library()
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+@register.inclusion_tag('posts/lastest_post.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+@register.inclusion_tag('posts/detail.html')
+def total_tags():
+    total_tags = Post.tags.all()
+    return {'total_tags': total_tags}
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
