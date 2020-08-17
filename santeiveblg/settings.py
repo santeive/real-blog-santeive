3748b3e2b4cb00d43f22454ef3957d396ad19728
santeive <packo.ive@gmail.com>
2020-08-17 12:22:10 -0500

    last changes

--- santeiveblg/settings.py
+++ santeiveblg/settings.py
@@ -7,11 +7,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# This is new:
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
